4a84df8de6b1cf3a68422c7b45adf115244ff863
gsusha <x.dyachenko@gmail.com>
2021-09-03 15:48:26 +0300

    menu background and anchors

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,25 @@
+//Меню бургер
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 	$('.header__list').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
-//Меню бургер
 
 
+//Изменение фона меню при прокрутке
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+		let wn = $(window).scrollTop();
 
-$("nav").on("click","a", function (event) {
-        event.preventDefault();
- 
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-
-        $('body,html').animate({scrollTop: top}, 800);
+		if(wn > 120){
+			$(".header").addClass("bg");
+		} else{
+			$(".header").removeClass("bg");
+		}
+	});
 });
-//Отвечает за плавное перемещение по якорям
---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Отвечает за плавное перемещение по якорям
+$("body").on("click","a", function (event) {
+		event.preventDefault();
+ 
+		let id  = $(this).attr('href'),
+			top = $(id).offset().top;
+
+		$('body,html').animate({scrollTop: top}, 800);
+});
